fe3bbb07247c278461755c7703faad712a72f84e
mHammy <133612424+mHammy@users.noreply.github.com>
2023-06-21 05:29:51 -0400

    Created a seperate script file containing the question array to slim down the function script file

---
+++ assets/js/questions.js
@@ -0,0 +1,111 @@
+var questionArray = [
+    {
+        numb: 1,
+        text: "1. Which of the following is correct about features of JavaScript?",
+        answer: "C. Both of the above.",
+        options: [
+            "A. JavaScript is is complementary to and integrated with HTML.",
+            "B. JavaScript is open and cross-platform.",
+            "C. Both of the above.",
+            "D. All of the above.",
+        ]
+    },    
+    {
+        numb: 2,
+        text: "2. How can you get the total number of arguments passed to a function?",
+        answer: "B. Using arguments.length property.",
+        options: [
+            "A. Using args.length property.",
+            "B. Using arguments.length property.",
+            "C. Both of the above.",
+            "D. None of the above.",
+        ]
+    },
+    {
+        numb: 3,
+        text: "3. Which of the following type of variable is visible only within a function where it is defined?",
+        answer: "B. local variable",
+        options: [
+            "A. global variable",
+            "B. local variable",
+            "C. Both of the above.",
+            "D. None of the above.",
+        ]
+    },
+    {
+        numb: 4,
+        text: "4. Which of the following function of Number object forces a number to display in exponential notation?",
+        answer: "A. toExponential()",
+        options: [
+            "A. toExponential()",
+            "B. toFixed()",
+            "C. toPrecision()",
+            "D. toLocaleString()",
+        ]
+    },
+    {
+        numb: 5,
+        text: "5. Which of the following function of Boolean object returns a string of either 'true' or 'false' depending upon the value of the object?",
+        answer: "C. toString()",
+        options: [
+            "A. toSource()",
+            "B. valueOf()",
+            "C. toString()",
+            "D. None of the above.",
+        ]
+    },
+    {
+        numb: 6,
+        text: "6. Which of the following function of String object returns a number indicating whether a reference string comes before or after or is the same as the given string in sort order",
+        answer: "A. localeCompare()",
+        options: [
+            "A. localeCompare()",
+            "B. search()",
+            "C. substr()",
+            "D. concat()",
+        ]
+    },
+    {
+        numb: 7,
+        text: "7. Which of the following function of String object returns the calling string value converted to upper case?",
+        answer: "B. toUpperCase()",
+        options: [
+            "A. toLocaleUpperCase()",
+            "B. toUpperCase()",
+            "C. toString()",
+            "D. substring()",
+        ]
+    },
+    {
+        numb: 8,
+        text: "8. Which of the following function of String object causes a string to be displayed in the specified size as if it were in a <font size = 'size'> tag?",
+        answer: "C. fontsize()",
+        options: [
+            "A. fixed()",
+            "B. fontcolor()",
+            "C. fontsize()",
+            "D. bold()",
+        ]
+    },
+    {
+        numb: 9,
+        text: "9. Which of the following function of Array object calls a function for each element in the array?",
+        answer: "D. forEach()",
+        options: [
+            "A. concat()",
+            "B. every()",
+            "C. filter()",
+            "D. forEach()",
+        ]
+    },
+    {
+        numb: 10,
+        text: "10. Which of the following function of Array object extracts a section of an array and returns a new array?",
+        answer: "C. slice()",
+        options: [
+            "A. reverse()",
+            "B. shift()",
+            "C. slice()",
+            "D. some()",
+        ]
+    }];
