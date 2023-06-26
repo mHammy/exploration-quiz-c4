3e663693f79653991784d9e156e3ad40cab44ae5
mHammy <133612424+mHammy@users.noreply.github.com>
2023-06-26 14:23:20 -0400

    Final update including the bug fix for the highscores page via js and css

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -1,30 +1,35 @@
 // Elements required
-scoreList = document.getElementById("score-list");
-retryButton = document.getElementById("retry-button");
+var scoreList = document.getElementById("score-list");
+var retryButton = document.getElementById("retry-button");
 
 // Grabbing the local storage information and throwing it in an array
 var scores = JSON.parse(localStorage.getItem("userScore")) || [];
 
-
-// sortScores();
+sortScores();
 displayScore();
 
-// function sortScores() {
-//     scores.sort(function (a, b) {
-//         return b.scores - a.scores;
-//     });
-    
-// };
+function sortScores() {
+    scores.sort(function (a, b) {
+        return b.highscore- a.highscore;
+    });
+};
 
 function displayScore() {
-
     for (i = 0; i < scores.length; i+= 1) {
-        var liTag = document.createElement("li")
-        liTag.textContent = (scores[i].highscore + " " + "|" + " " + scores[i].initials);
-         scoreList.appendChild(liTag);
+        var liTag = document.createElement("li");
+        var pScore = document.createElement("p");
+        var pInitial = document.createElement("p");
+        pScore.classList.add("score-side");
+        pInitial.classList.add("initial-side");
+        pScore.textContent = (scores[i].highscore);
+        pInitial.textContent = (scores[i].initials);
+        liTag.append(pScore, pInitial);
+        scoreList.appendChild(liTag);
+        
     }
 
 };
+
 
 retryButton.addEventListener("click", function(event) {
     event.preventDefault();
